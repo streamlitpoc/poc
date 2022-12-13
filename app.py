79da602843337f72a1419e02758cdf5d86c1e153
streamlitpoc <117904355+streamlitpoc@users.noreply.github.com>
2022-12-13 16:34:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,12 @@ def main():
     ('Operations & Monitoring', 'Sales'))
 
     if option == 'Operations & Monitoring':
-        source_path = 'C:\\Users\\maniss7\\Downloads\\Streamlit_poc\\Demo\\data\\operation_data.csv'
+        source_path = './operation_data.csv'
         op.operation_dashboard(source_path)
         # content = '<p style="font-family:Courier; color:Black; font-size: 20px;">This dashboard gives you insights for job monitoring in <strong>PRODUCTION</strong> enviroment. Support Engineer can you this dashboard for jobs monitoring, raising defects and fixing bugs.</p>'
         # st.sidebar.markdown (content, unsafe_allow_html=True)
     else:
-        source_path = 'C:\\Users\\maniss7\\Downloads\\Streamlit_poc\\sales_data_sample.csv'
+        source_path = './sales_data_sample.csv'
         content = '<p style="font-family:Courier; color:Black; font-size: 20px;">This dashboard gives you insights for <strong>Sales</strong>.User can find different insights related to daily sales, visitor traffic, total revenue and lot more ..</p>'
         st.sidebar.markdown (content, unsafe_allow_html=True)
         sa.sales(source_path)
