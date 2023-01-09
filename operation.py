c9897a9e73a85c5b8f7e6277c8624776ad69d576
streamlitpoc <117904355+streamlitpoc@users.noreply.github.com>
2023-01-09 11:59:48 +0530

    Update operation.py

--- operation.py
+++ operation.py
@@ -20,7 +20,7 @@ def operation_dashboard(source_data):
     unsafe_allow_html=True
         )
     df = pd.read_csv(source_data,encoding="utf-8")
-    df = ps.sqldf("select * from df where substring(start_date_time,0,8) in ('2022-12')")
+    df = ps.sqldf("select * from df where substring(start_date_time,0,8) in ('2022-11','2022-12')")
     option = st.sidebar.selectbox('END DATE TIME',('YEAR', 'MONTH', 'DAY'))
     if option == 'YEAR':
         year_df = ps.sqldf("select distinct substring(start_date_time,0,5) as year from df")
